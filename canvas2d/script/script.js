8364eb9262f32e8bae764db8a55e9c58e56d4b64
tyamap <tom333my@outlook.com>
2020-07-22 17:02:24 +0900

    add sample ベジェ曲線の描画

--- canvas2d/script/script.js
+++ canvas2d/script/script.js
@@ -17,6 +17,7 @@
     let red = 'rgba(255, 0, 0, 0.3)';
     let green = 'rgba(0, 255, 0, 0.3)';
     let blue = 'rgba(0, 0, 255, 0.3)';
+    let purple = 'rgba(154, 18, 179, 1)';
 
     /**
      * ページのロードが完了したときに発火する load イベント
@@ -68,6 +69,23 @@
                     let endRadian = Math.random() * Math.PI * 2.0;
                     // 扇形の描画処理を行う
                     drawFan(200, 200, 100, startRadian, endRadian, blue);
+                    break;
+                case 'btn007':
+                    // 二次ベジェ曲線を描画する
+                    drawQuadraticBezier(
+                        100, 100, // 始点
+                        100, 300, // 終点
+                        300, 200, // 制御点
+                        purple
+                    );
+                    // 三次ベジェ曲線を描画する
+                    drawCubicBezier(
+                        300, 100, // 始点
+                        300, 300, // 終点
+                        500, 0, // 始点の制御点
+                        500, 400, // 終点の制御点
+                        purple
+                    );
                     break;
             }
         });
@@ -207,6 +225,69 @@
         ctx.closePath();
         // 設定したパスで扇形の描画を行う
         ctx.fill();
+    }
+
+        /**
+     * 線分を二次ベジェ曲線で描画する
+     * @param {number} x1 - 線分の始点の X 座標
+     * @param {number} y1 - 線分の始点の Y 座標
+     * @param {number} x2 - 線分の終点の X 座標
+     * @param {number} y2 - 線分の終点の Y 座標
+     * @param {number} cx - 制御点の X 座標
+     * @param {number} cy - 制御点の Y 座標
+     * @param {string} [color] - 線を描画する際の色
+     * @param {number} [width=1] - 線幅
+     */
+    function drawQuadraticBezier(x1, y1, x2, y2, cx, cy, color, width = 1){
+        // 色が指定されている場合はスタイルを設定する
+        if(color != null){
+            ctx.strokeStyle = color;
+        }
+        // 線幅を設定する
+        ctx.lineWidth = width;
+        // パスの設定を開始することを明示する
+        ctx.beginPath();
+        // パスの始点を設定する
+        ctx.moveTo(x1, y1);
+        // 二次ベジェ曲線の制御点と終点を設定する
+        ctx.quadraticCurveTo(cx, cy, x2, y2);
+        // パスを閉じることを明示する
+        ctx.closePath();
+        // 設定したパスで線描画を行う
+        ctx.stroke();
+    }
+
+    /**
+     * 線分を三次ベジェ曲線で描画する
+     * @param {number} x1 - 線分の始点の X 座標
+     * @param {number} y1 - 線分の始点の Y 座標
+     * @param {number} x2 - 線分の終点の X 座標
+     * @param {number} y2 - 線分の終点の Y 座標
+     * @param {number} cx1 - 始点の制御点の X 座標
+     * @param {number} cy1 - 始点の制御点の Y 座標
+     * @param {number} cx2 - 終点の制御点の X 座標
+     * @param {number} cy2 - 終点の制御点の Y 座標
+     * @param {string} [color] - 線を描画する際の色
+     * @param {number} [width=1] - 線幅
+     */
+    function drawCubicBezier(x1, y1, x2, y2, cx1, cy1, cx2, cy2, color, width = 1){
+        // 色が指定されている場合はスタイルを設定する
+        if(color != null){
+            ctx.strokeStyle = color;
+        }
+        // 線幅を設定する
+        ctx.lineWidth = width;
+        // パスの設定を開始することを明示する
+        ctx.beginPath();
+        // パスの始点を設定する
+        ctx.moveTo(x1, y1);
+        // 三次ベジェ曲線の制御点と終点を設定する
+        ctx.bezierCurveTo(cx1, cy1, cx2, cy2, x2, y2);
+        // 設定したパスで線描画を行う
+        // closePathの前に実行することで、線を閉じずに描画できる
+        ctx.stroke();
+        // パスを閉じることを明示する
+        ctx.closePath();
     }
 
     /**
